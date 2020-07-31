06072afc2f64fc894b6c0491f362362361717dd8
caleb Omoijuanfo <c.omoijuanfo@gmail.com>
2020-07-31 13:45:54 +0100

    Updates

---
+++ precache-manifest.fd78360c9d638e8c4a14762b4d0ac51a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d7ff5b8871d73a79e5614a1999c49e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2c23c1a0dffe6e1f0418",
+    "url": "/monsters-rolodex/static/css/main.4e7038d1.chunk.css"
+  },
+  {
+    "revision": "440b54c73a1fd6396f6b",
+    "url": "/monsters-rolodex/static/js/2.f101649a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.f101649a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c23c1a0dffe6e1f0418",
+    "url": "/monsters-rolodex/static/js/main.42a2373e.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  }
+]);
